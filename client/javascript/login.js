9af9d82d0ae1eedc12f54b428efb605e74fb24da
jibjiby <my01sun@gmail.com>
2022-05-19 16:13:05 +0900

    update: login 페이지 수정

--- client/javascript/login.js
+++ client/javascript/login.js
@@ -1,22 +1,71 @@
-$('button').click(function (e) {
-  e.preventDefault();
-  console.log('button click');
-
-  // let emailInput = $('#email').val();
-  // let passwordInput = $('#password').val();
-
-  // console.log('------입력한 값------');
-  // console.log(emailInput);
-  // console.log(passwordInput);
-
-  //  TODO: post 요청 보내기
-  // 참고 : https://api.jquery.com/jquery.post/
-  // https://araikuma.tistory.com/640
-  // $.post('/api/login', function () {});
-
-  $.get('/hi', function (result) {
-    console.log(result);
+$('#signup').click(function () {
+  $('#first').fadeOut('fast', function () {
+    $('#second').fadeIn('fast');
   });
+});
 
-  return false;
+$('#signin').click(function () {
+  $('#second').fadeOut('fast', function () {
+    $('#first').fadeIn('fast');
+  });
+});
+
+$(function () {
+  $("form[name='login']").validate({
+    rules: {
+      email: {
+        required: true,
+        email: true,
+      },
+      password: {
+        required: true,
+      },
+    },
+    messages: {
+      email: '이메일을 입력해주세요.',
+
+      password: {
+        required: '비밀번호를 입력해주세요.',
+      },
+    },
+    submitHandler: function (form) {
+      form.submit();
+    },
+  });
+});
+
+$(function () {
+  $("form[name='registration']").validate({
+    rules: {
+      email: {
+        required: true,
+        email: true,
+      },
+      password: {
+        required: true,
+        minlength: 5,
+      },
+      password_confirm: {
+        required: true,
+        minlength: 5,
+      },
+    },
+
+    messages: {
+      password: {
+        required: '비밀번호를 입력해주세요.',
+        minlength: '최소 5글자이상으로 입력해주세요.',
+      },
+      password_confirm: {
+        required: '비밀번호를 입력해주세요.',
+        minlength: '최소 5글자이상으로 입력해주세요.',
+        equalTo: '동일한 비밀번호를 입력해주세요.',
+      },
+      email: '이메일을 입력해주세요.',
+    },
+
+    submitHandler: function (form) {
+      form.submit();
+    },
+  });
 });
